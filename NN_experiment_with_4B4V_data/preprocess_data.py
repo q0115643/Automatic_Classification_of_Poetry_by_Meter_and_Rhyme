46fef0ce6a8c7a60e5656479524f45aa6a9fd150
rokrokss <q0115643@kaist.ac.kr>
2018-05-19 11:44:16 +0900

    data align in vertical & horizontal format

--- NN_experiment_with_4B4V_data/preprocess_data.py
+++ NN_experiment_with_4B4V_data/preprocess_data.py
@@ -44,18 +44,24 @@ with open("syllables.txt") as syllable_txt, open("lines.txt") as lines_txt:
             if syllables[j]==' ' and line[i]!=' ':
                 j += 1
             if line[i]==' ' and syllables[j]==' ':
-                syllables = syllables[:j] + ' ' + WB + syllables[j:]
-                j += len(WB)
+                syllables = syllables[:j] + WB + ' ' + WB + syllables[j+1:]
+                j += len(WB) + 1
             elif line[i]==' ':
-                syllables = syllables[:j] + WB + syllables[j:]
-                j += len(WB) - 1
+                syllables = syllables[:j] + WB + WB + syllables[j:]
+                j += len(WB)
             j += 1
             i += 1
-        syllables = WB + ' ' + syllables + ' ' + WB
+        syllables = WB + syllables + WB
 
         print("***after insertion of Word Boundary marker***")
         print("syllables: {0}\nline: {1}\n".format(syllables, line))
+        # horizontal align
         processed_syllables.write(syllables + '\n')
+        # vertical align
+        # syllables_list = re.findall(r'\S+', syllables)
+        # for syllable in syllables_list:
+        #     processed_syllables.write(syllable + '\n')
+        # processed_syllables.write('\n')
 processed_syllables.close()
 syllable_txt.close()
 lines_txt.close()
@@ -69,6 +75,12 @@ with open('4B4V.csv', newline='') as myFile:
     reader = csv.reader(myFile)
     for row in reader:
         line = row[1]
+        # for horizontal align
         processed_meters.write(line + '\n')
+        # for vertical align
+        # for c in line:
+        #     processed_meters.write(c + '\n')
+        # processed_meters.write('\n')
+
 processed_meters.close()
 myFile.close()
