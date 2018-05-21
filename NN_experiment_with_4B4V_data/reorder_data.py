40aeb809b90ee8bfcc36ec9d1b4aceab03563602
rokrokss <q0115643@kaist.ac.kr>
2018-05-21 19:53:59 +0900

    update parser and 4B4V dataset

--- NN_experiment_with_4B4V_data/reorder_data.py
+++ NN_experiment_with_4B4V_data/reorder_data.py
@@ -1,10 +1,11 @@
 input_data = open('input.txt', 'w')
 
-with open("processed_meters.txt") as meters_txt, open("processed_syllables.txt") as syllables_txt:
-    for meters, syllables in zip(meters_txt, syllables_txt):
+with open("processed_meters.txt") as meters_txt, open("processed_syllables.txt") as syllables_txt, open("lines_4B4V.txt") as lines_txt:
+    for meters, syllables, line in zip(meters_txt, syllables_txt, lines_txt):
         len_meters = len(meters) - 1
         len_syllables = len(syllables.split())
         if len_meters != len_syllables:
+            print(line)
             print(syllables)
         print('# of meters: ' + str(len_meters))
         print('# of syllables: ' + str(len_syllables) + '\n')
