c36e2c1448e6020bbe22c131e02edbc547970f5e
rokrokss <q0115643@kaist.ac.kr>
2018-05-22 17:27:32 +0900

    done data parsing and preprocess to get input

---
+++ NN_experiment_with_4B4V_data/reorder_4B4V.py
@@ -0,0 +1,38 @@
+import csv
+import re
+
+
+'''
+reorder ../4B4V.csv data to make the input file for Bi-RNN+CRF model
+'''
+cnt = 0
+input_data = open('input.txt', 'w')
+with open('../4B4V.csv', newline='') as myFile:
+    reader = csv.reader(myFile)
+    for row in reader:
+        syllables = row[0]
+        origin = syllables
+        syllables = re.sub('[\']+', '', syllables)
+        syllables = re.sub('[^a-zA-Z ]+', ' ', syllables)
+        syllables = re.sub("\s\s+", " ", syllables)
+        syllables = syllables.strip()
+        syllables = syllables.lower()
+        meters = row[1]
+        meters = meters.strip()
+        syllables_list = re.findall(r'\S+', syllables)
+        syllables_num = len(syllables_list)
+        meters_num = len(meters)
+        if syllables_num != meters_num:
+            print(syllables)
+            print(origin)
+            print(meters)
+            print('\n')
+            cnt += 1
+        # vertical align
+        for syllable, meter in zip(syllables_list, meters):
+            input_data.write(syllable + ' ' + meter + '\n')
+        input_data.write('\n')
+
+print("lines with not-matching number of meters and syllables: " + str(cnt))
+input_data.close()
+myFile.close()
