b5aa109b5fc3ef12d26cf49be60e9d7c63980c62
rokrokss <q0115643@kaist.ac.kr>
2018-05-23 06:57:31 +0900

    BiLSTM-CRF done

--- NN_experiment_with_4B4V_data/reorder_4B4V.py
+++ NN_experiment_with_4B4V_data/reorder_4B4V.py
@@ -1,15 +1,29 @@
 import csv
 import re
-
+import random
 
 '''
-reorder ../4B4V.csv data to make the input file for Bi-RNN+CRF model
+split and reorder ../4B4V.csv data to make the input file for Bi-RNN+CRF model
 '''
 cnt = 0
-input_data = open('input.txt', 'w')
+file_length = 0
+train_data = open('./tagger_theano/train.txt', 'w')
+dev_data = open('./tagger_theano/dev.txt', 'w')
+test_data = open('./tagger_theano/test.txt', 'w')
 with open('../4B4V.csv', newline='') as myFile:
     reader = csv.reader(myFile)
     for row in reader:
+        syllables = row[0]
+        file_length += 1
+print("file size: " + str(file_length))
+test_size = file_length//10
+print("test & dev size: " + str(test_size))
+myFile.close()
+file_idx = 0
+with open('../4B4V.csv', newline='') as myFile:
+    reader = csv.reader(myFile)
+    for row in reader:
+        file_idx += 1
         syllables = row[0]
         origin = syllables
         syllables = re.sub('[\']+', '', syllables)
@@ -30,13 +44,25 @@ with open('../4B4V.csv', newline='') as myFile:
             cnt += 1
         # vertical align
         for syllable, meter in zip(syllables_list, meters):
-            if meter == '+':
-                meter = 's'
+            if meter == '^':
+                b = random.randint(0, 1)
+                tag_list = ['-', '+']
+                meter = tag_list[b]
+            if file_idx <= test_size:
+                test_data.write(syllable + ' ' + meter + '\n')
+            elif file_idx <= 2*test_size:
+                dev_data.write(syllable + ' ' + meter + '\n')
             else:
-                meter = 'u'
-            input_data.write(syllable + ' ' + 'I-' + meter + '\n')
-        input_data.write('\n')
+                train_data.write(syllable + ' ' + meter + '\n')
+        if file_idx <= test_size:
+            test_data.write('\n')
+        elif file_idx <= 2*test_size:
+            dev_data.write('\n')
+        else:
+            train_data.write('\n')
 
 print("lines with not-matching number of meters and syllables: " + str(cnt))
-input_data.close()
 myFile.close()
+test_data.close()
+train_data.close()
+dev_data.close()
